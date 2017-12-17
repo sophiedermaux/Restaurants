14cd325e40531b100a97b703854153f815502d23
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-12-17 18:43:23 +0000

    made some slight changes to css andand fixed show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,30 +1,17 @@
 class RestaurantsController < ApplicationController
-   RESTAURANTS = {
-    1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
-    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" },
-    3 => { name: "Andina", address: "Shoreditch, London", category: "peruvian" },
-    4 => { name: "Franco Manca", address: "Clapham Junction, London", category: "italian" },
-    5 => { name: "Mama Lan", address: "Brixton, London", category: "chinese" },
-    6 => { name: "Dumplings Legend", address: "China Town, London", category: "chinese" },
-    7 => { name: "Som Saa ", address: "Shoreditch, London", category: "thai" },
-  }
 
   def show
-    @restaurant = RESTAURANTS[params[:id].to_i]
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def index
-    if params[:food_type].blank?
-      @restaurants = RESTAURANTS
-    else
-      @category = params[:food_type]
-      @restaurants = RESTAURANTS.select { |id, restaurant| restaurant[:category] == @category }
-    end
+    @restaurants = Restaurant.all
+    @category = params[:food_type]
+    @restaurants = Restaurant.select { |id, restaurant| restaurant[:category] == @category }
   end
-
-  def create
-
-  end
-
 end
+
+
+
+
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Dishoom", address: "Shoreditch", category: "Indian")
+Restaurant.create(name: "Gun Powder", address: "Shoreditch", category: "Indian")
+Restaurant.create(name: "Zumbura", address: "Clapham", category: "Indian")
+Restaurant.create(name: "Dumplings Legend", address: "Soho", category: "Chinese")
+Restaurant.create(name: "Min Jiang", address: "Kensington", category: "Chinese")
+Restaurant.create(name: "Hunan", address: "Pimlico", category: "Chinese")
