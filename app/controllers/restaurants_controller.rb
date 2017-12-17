cf747bd27d49e1376ef9f53bd0899dfd37affa1d
Sophie Dermaux <sophiebcdermaux@gmail.com>
2017-12-17 13:04:27 +0000

    adding restaurant model

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,11 @@ class RestaurantsController < ApplicationController
    RESTAURANTS = {
     1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
     2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+    3 => { name: "Andina", address: "Shoreditch, London", category: "peruvian" },
+    4 => { name: "Franco Manca", address: "Clapham Junction, London", category: "italian" },
+    5 => { name: "Mama Lan", address: "Brixton, London", category: "chinese" },
+    6 => { name: "Dumplings Legend", address: "China Town, London", category: "chinese" },
+    7 => { name: "Som Saa ", address: "Shoreditch, London", category: "thai" },
   }
 
   def show
